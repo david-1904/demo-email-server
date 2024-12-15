9524af83e176682083780d642776dc444dc39ca6
David Heuckmann <david.heuckmann@gmail.com>
2024-12-15 22:32:43 +0100

    feat: use EmailStatus enum, specify the mapped columns; use @Lob as email body can be large

--- src/main/java/org/demo/entity/Email.java
+++ src/main/java/org/demo/entity/Email.java
@@ -4,31 +4,36 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
 @Data
 public class Email {
 
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long emailId;
 
+    @Column(name = "email_from", nullable = false)
     private String emailFrom;
 
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    @JoinColumn(name = "email_id")
-    private List<Recipient> emailTo;
-
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    @JoinColumn(name = "email_id")
-    private List<Recipient> emailCC;
-
+    @Lob
     private String emailBody;
 
-    private Integer state;
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "email_status")
+    private EmailStatus state;
 
-    private Timestamp createdAt;
-    private Timestamp updatedAt;
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "email_id")
+    private List<Recipient> recipients;
 
 }
