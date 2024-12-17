8928f84e9ca5cfe42fbb499d0ef8bf0ccdb903bd
David Heuckmann <david.heuckmann@gmail.com>
2024-12-17 21:55:56 +0100

    feat: add repository method to find email by sender

--- src/main/java/org/demo/repository/EmailRepository.java
+++ src/main/java/org/demo/repository/EmailRepository.java
@@ -4,6 +4,10 @@ import org.demo.entity.Email;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EmailRepository extends JpaRepository<Email, Long> {
+    Optional<Email> findByEmailFrom(String email);
 }
+
