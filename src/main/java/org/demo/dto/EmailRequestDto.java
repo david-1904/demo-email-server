650939bef5ccdbc161b0aa6c627718533be99e71
David Heuckmann <david.heuckmann@gmail.com>
2024-12-17 21:55:13 +0100

    feat: add subject and remove state as we handle state internally

--- src/main/java/org/demo/dto/EmailRequestDto.java
+++ src/main/java/org/demo/dto/EmailRequestDto.java
@@ -1,9 +1,9 @@
-package org.demo.entity.dto;
+package org.demo.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-import org.demo.entity.enums.EmailStatus;
+import org.demo.entity.enums.EmailState;
 
 import java.util.List;
 
@@ -13,15 +13,15 @@ public class EmailRequestDto {
     @NotNull(message = "Sender email cannot be null")
     private String emailFrom;
 
+    @NotNull(message = "Subject cannot be null")
+    private String subject;
+
     private String emailBody;
 
     @NotNull(message = "Recipients cannot be null")
     private List<RecipientsDto> emailTo;
 
     private List<RecipientsDto> emailCC;
-
-    @NotNull(message = "State cannot be null")
-    private EmailStatus state;
 
     @Data
     public static class RecipientsDto {
