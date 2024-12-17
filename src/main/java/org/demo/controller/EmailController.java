c10f16557a4685cfb0e7b8a0311ac8d38f002558
David Heuckmann <david.heuckmann@gmail.com>
2024-12-17 21:57:05 +0100

    refactor: use correct responses for different actions (e.g. 204 for delete) - add actions for send and delete

--- src/main/java/org/demo/controller/EmailController.java
+++ src/main/java/org/demo/controller/EmailController.java
@@ -1,10 +1,13 @@
 package org.demo.controller;
 
+import org.demo.dto.EmailResponseDto;
 import org.demo.entity.Email;
-import org.demo.entity.dto.EmailRequestDto;
+import org.demo.dto.EmailRequestDto;
 import org.demo.service.EmailService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/emails")
@@ -17,15 +20,32 @@ public class EmailController {
     }
 
     @PostMapping
-    public ResponseEntity<String> createEmail(@RequestBody EmailRequestDto emailRequest) {
-        emailService.saveEmail(emailRequest);
-        return ResponseEntity.ok("Email created successfully!");
+    public ResponseEntity<EmailResponseDto> createEmail(@RequestBody EmailRequestDto email) {
+        Email createdEmail = emailService.saveEmail(email);
+        EmailResponseDto responseDto = emailService.mapToEmailResponseDto(createdEmail);
+        URI location = URI.create("/emails/" + createdEmail.getEmailId());
+        return ResponseEntity.created(location).body(responseDto);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Email> updateEmail(@PathVariable Long id, 
-                                             @RequestBody Email updatedEmail) {
-        return ResponseEntity.ok(emailService.updateEmail(id, updatedEmail));
+    public ResponseEntity<EmailResponseDto> updateEmail(@PathVariable Long id,
+                                             @RequestBody Email email) {
+        Email updatedEmail = emailService.updateEmail(id, email);
+        EmailResponseDto responseDto = emailService.mapToEmailResponseDto(updatedEmail);
+
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEmail(@PathVariable Long id) {
+        emailService.deleteEmail(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}/send")
+    public ResponseEntity<String> sendEmail(@PathVariable Long id) {
+        emailService.sendEmail(id);
+        return ResponseEntity.ok("Email sent successfully!");
     }
     
 }
