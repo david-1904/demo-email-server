24626934552d1982e746baa43080136e3debc9e7
David Heuckmann <david.heuckmann@gmail.com>
2024-12-20 10:40:42 +0100

    chore: optimize imports and add postgres test configuration

--- src/test/java/org/demo/EmailServerAppApplicationTests.java
+++ src/test/java/org/demo/EmailServerAppApplicationTests.java
@@ -1,10 +1,32 @@
 package org.demo;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @SpringBootTest
 class EmailServerAppApplicationTests {
+
+    static final PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:latest")
+            .withDatabaseName("email_server_test")
+            .withUsername("admin")
+            .withPassword("secret");
+
+    @DynamicPropertySource
+    static void configureTestContainer(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgresContainer::getUsername);
+        registry.add("spring.datasource.password", postgresContainer::getPassword);
+    }
+
+    @BeforeAll
+    static void startContainer() {
+        postgresContainer.start();
+    }
 
     @Test
     void contextLoads() {
