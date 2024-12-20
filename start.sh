b083af4e08fc18d9f43e08b8f94e89eb5581489c
David Heuckmann <david.heuckmann@gmail.com>
2024-12-20 10:33:10 +0100

    feat: add jacoco report to start script; remove test environment as testcontainers are used

--- start.sh
+++ start.sh
@@ -10,20 +10,11 @@ docker compose down --volumes --remove-orphans
 echo "Starting production environment..."
 docker compose up --build -d postgres flyway email-server-app
 
-# Start test services
-echo "Setting up test environment..."
-docker compose up --build -d postgres_test flyway_test
-
 # Run tests
 echo "Running tests..."
-SPRING_PROFILES_ACTIVE=test \
-SPRING_DATASOURCE_URL=jdbc:postgresql://localhost:5433/email_server_test \
-SPRING_DATASOURCE_USERNAME=admin \
-SPRING_DATASOURCE_PASSWORD=secret \
 mvn test || { echo "Tests failed! Cleaning up..."; docker compose down; exit 1; }
 
-# Stop test services
-echo "Stopping test environment..."
-docker compose stop postgres_test flyway_test
+# Jacoco Report
+mvn jacoco:report
 
 echo "Production environment is still running."
