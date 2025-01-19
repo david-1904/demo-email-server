e475cd9ea9d7d960a6d2f3fefb0bc631262a66b8
David Heuckmann <david.heuckmann@gmail.com>
2025-01-19 20:12:27 +0100

    feat: adjust start script so that also containers outside of compose.yaml are stopped and removed. This resolves port issues with different applications

--- start.sh
+++ start.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
+echo "Stopping and removing all running containers..."
+# Get IDs of all running Docker containers
+running_containers=$(docker ps -q)
+
+# Check if there are any running containers
+if [ -n "$running_containers" ]; then
+  # Stop all running containers using their IDs
+  docker stop $running_containers
+fi
+
+# Get IDs of all existing Docker containers (both running and stopped)
+all_containers=$(docker ps -aq)
+
+# Check if there are any containers (running or stopped)
+if [ -n "$all_containers" ]; then
+  # Remove all containers using their IDs
+  docker rm $all_containers
+fi
+
+# Remove all unused Docker networks
+if ! docker network prune -f; then
+  echo "Failed to prune Docker networks. Either there are no unused networks or Docker is not running."
+fi
+
 # Build the project
 mvn clean package -DskipTests || { echo "Build failed! Exiting."; exit 1; }
-
-# Stop all running containers
-docker compose down --volumes --remove-orphans
 
 # Start production services
 echo "Starting production environment..."
